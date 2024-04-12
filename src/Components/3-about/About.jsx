5c9363391bd81f066e3fadcf223acc4545e79710
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-12 14:13:02 +0000

    about section

--- src/Components/3-about/About.jsx
+++ src/Components/3-about/About.jsx
@@ -1,8 +1,34 @@
-
+import "./about.css";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import about from "/./public/about.jpg";
 function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <div className="aboutContent">
+          <h3 className="h3">Italian Restaurant</h3>
+          <div className="contenu">
+          
+          <h1>WELCOME</h1>
+          <p>
+            Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est.Sed <br />
+            fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi,ac <br />
+            facilisis ligula sem id neque.
+          </p>
+
+          <a href="/" className="arrowLink">
+            OUR STORY
+          
+          <FontAwesomeIcon className="iconAr" icon={faArrowRight} />
+          </a>
+        </div>
+          
+            <img className="imgAbout" src={about} alt="" />
+         
+        
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
