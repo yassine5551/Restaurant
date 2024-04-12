0bd476c01e4226758fd65d68851e7740b1259ba8
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-12 17:02:26 +0000

    styling the card

--- src/Components/cards/Card.jsx
+++ src/Components/cards/Card.jsx
@@ -11,24 +11,27 @@ function Card() {
               <div className="card">
                   <img src={card1} alt="" />
                   <h3>ROMANTIC RESTAURANT</h3>
-                  <p>Phasellus lorem enim, luctus ut velit eget, con-vallis egestas eros.</p>
+                  <p>Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis egestas eros.</p>
                   <a href="">LEARN MORE  <FontAwesomeIcon className="iconAr" icon={faArrowRight} /></a>
               </div>
               <div className="card">
                   <img src={card2} alt="" />
                   <h3>DELICIOUS FOOD</h3>
-                  <p>Phasellus lorem enim, luctus ut velit eget, con-vallis egestas eros.</p>
+                  <p>Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis egestas eros.</p>
                   <a href="">LEARN MORE  <FontAwesomeIcon className="iconAr" icon={faArrowRight} /></a>
               </div>
               <div className="card">
                   <img src={card3} alt="" />
                   <h3>RED WINES YOU LOVE</h3>
-                  <p>Phasellus lorem enim, luctus ut velit eget, con-vallis egestas eros.</p>
+                  <p>Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis egestas eros.</p>
                   <a href="">LEARN MORE  <FontAwesomeIcon className="iconAr" icon={faArrowRight} /></a>
               </div>
           </div>
       </>
   )
 }
+
+
+
 
 export default Card
