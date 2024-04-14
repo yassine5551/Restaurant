5a16b6c46368097db96d8d086f1cc7cae2ad6f3a
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-14 22:47:22 +0000

    hamburger fucntion works properly

--- src/Components/1-header/Header.jsx
+++ src/Components/1-header/Header.jsx
@@ -1,36 +1,61 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect,useRef } from "react";
 import "./header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import logo from '/./public/logoNobg.png';
-
-
+import { faBars,faTimes } from "@fortawesome/free-solid-svg-icons";
+import logo from "/./public/logoNobg.png";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-
+  const menuRef = useRef(null); // Ref for the menu box
+// for scrolling
   useEffect(() => {
     const handleScroll = () => {
       const isScrolled = window.scrollY > 0;
       setScrolled(isScrolled);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  // for canceling the menu box
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsMenuOpen(false);
+        }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+    };
+}, []);
+  
+  const toggleMenu = () => {
+        
+        setIsMenuOpen(!isMenuOpen);
+        
+  };
+  
+
 
   return (
     <>
-      <header className={`header  ${scrolled ? 'scrolled' : ''}`}>
-        <div className="logo"><img src={logo} alt="" /></div>
+      <header className={`header  ${scrolled ? "scrolled" : ""}`}>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
 
-        <ul className="links ">
+        <ul ref={menuRef} className={`links ${isMenuOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={toggleMenu}><FontAwesomeIcon icon={faTimes} className="close-box" /></button>
           <li>
             <a href="">home</a>
           </li>
@@ -52,14 +77,17 @@ function Header() {
           <li>
             <a href="">contact</a>
           </li>
+          
         </ul>
         <div className="icons">
           <FontAwesomeIcon className="icon" icon={faFacebookF} />
           <FontAwesomeIcon className="icon" icon={faInstagram} />
           <FontAwesomeIcon className="icon" icon={faTwitter} />
-          <FontAwesomeIcon className='icon menu' icon={faBars} />
-          
-          
+          <FontAwesomeIcon
+            className="icon menu"
+            icon={faBars}
+            onClick={toggleMenu}
+          />
         </div>
       </header>
     </>
