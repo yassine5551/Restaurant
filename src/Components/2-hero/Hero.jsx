3c6323cbf0a78c7174e090e52a788fb2cbdad670
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-18 21:57:25 +0000

    switching from css into tailwandcss and making hero section responsive in all devices ..

--- src/Components/2-hero/Hero.jsx
+++ src/Components/2-hero/Hero.jsx
@@ -1,15 +1,15 @@
 import heroImg2 from '/./public/heroImg2.jpg';
-import "./hero.css";
+
 function Hero() {
   
   return (
     <>
-      <div className="containe">
+      <div className="relative flex  justify-center items-center ">
         <img src={heroImg2} alt="" />
-        <div className='content'>
-        <h1 className='welcome'>Welcome to</h1>
-        <p className='para'>pato place</p>
-        <a className='btn' href="/" >LOOk MENU</a>
+        <div className='absolute text-white uppercase text-center'>
+        <h1 className='text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian '>Welcome to</h1>
+        <p className='text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 '>pato place</p>
+        <a className='text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light ' href="/" >LOOk MENU</a>
         </div>
       </div>
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'italian' :['Courgette']
+    }
   },
   plugins: [
      '@tailwindcss/forms'
