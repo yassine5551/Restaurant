7e06309c103066f52d70a979d96a6f9ea65e9613
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-19 18:20:40 +0000

    tailwandcss on cards section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Bar from './Components/0-bar_top/Bar';
 import Header from "./Components/1-header/Header"
 import Hero from "./Components/2-hero/Hero"
 import About from "./Components/3-about/About"
-// import Reservation from "./Components/5-reservation/Reservation"
-// import Footer from "./Components/8-footer/Footer"
-import Card from './Components/cards/Card';
+// import Reservation from "./Components/6-reservation/Reservation"
+// import Footer from "./Components/7-footer/Footer"
+import Card from './Components/5-cards/Card';
 import Discorver from './Components/4-discorver/Discorver';
 import './index.css'
 
---
+++ src/Components/5-cards/Card.jsx
@@ -0,0 +1,70 @@
+import "./card.css";
+import card1 from "/./public/card1.jpg";
+import card2 from "/./public/card2.jpg";
+import card3 from "/./public/card3.jpg";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function Card() {
+  return (
+    <>
+      <div className="mt-[100px] flex justify-evenly items-center gap-8">
+        <div className="flex-col text-center gap-5 ">
+          <img
+            src={card1}
+            alt="img1"
+            className="w-[400px] h-[250px] rounded-[15px] cursor-pointer "
+          />
+          <h3 className="mt-[20px] tracking-[5px] font-medium leading-[10px] my-[5px] text-red-600">
+            ROMANTIC RESTAURANT
+          </h3>
+          <p className="mb-1">
+            Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis
+            egestas eros.
+          </p>
+          <a href="" className="text-black w-fit hover:text-red-600">
+            LEARN MORE{" "}
+            <FontAwesomeIcon className="iconAr" icon={faArrowRight} />
+          </a>
+        </div>
+        <div className="flex-col text-center gap-5">
+          <img
+            src={card2}
+            alt="img2"
+            className="w-[400px] h-[250px] rounded-[15px] cursor-pointer "
+          />
+          <h3 className="mt-[20px] tracking-[5px] font-medium leading-[10px] my-[5px] text-red-600">
+            DELICIOUS FOOD
+          </h3>
+          <p className="mb-1">
+            Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis
+            egestas eros.
+          </p>
+          <a href="" className="text-black w-fit hover:text-red-600">
+            LEARN MORE{" "}
+            <FontAwesomeIcon className="iconAr" icon={faArrowRight} />
+          </a>
+        </div>
+        <div className="flex-col text-center gap-5">
+          <img
+            src={card3}
+            alt="img3"
+            className="w-[400px] h-[250px] rounded-[15px] cursor-pointer "
+          />
+          <h3 className="mt-[20px] tracking-[5px] font-medium leading-[10px] my-[5px] text-red-600">
+            RED WINES YOU LOVE
+          </h3>
+          <p className="mb-1">
+            Phasellus lorem enim, luctus ut velit eget, <br /> con-vallis
+            egestas eros.
+          </p>
+          <a href="" className="text-black w-fit hover:text-red-600">
+            LEARN MORE
+            <FontAwesomeIcon className="iconAr" icon={faArrowRight} />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
