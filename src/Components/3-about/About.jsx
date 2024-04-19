e2c12be661988cb980d633f2f27630804e915ef8
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-19 04:27:17 +0000

    fixing resposive in some devices

--- src/Components/3-about/About.jsx
+++ src/Components/3-about/About.jsx
@@ -5,34 +5,50 @@ import about2 from "/./public/about2.jpg";
 function About() {
   return (
     <>
-      <div className="flex relative mt-[150px] xl:justify-evenly">
-          
-          <div className="flex justify-center items-center flex-col">
+      <div
+        className="flex flex-col-reverse lg:flex-row mt-[75px] xl:justify-evenly gap-6
+      "
+      >
+        <div className="flex justify-center items-center flex-col gap-3">
           <h3
-            className="absolute top-16 font-bold uppercase tracking-wide 
+            className=" font-bold uppercase tracking-wide 
              font-italian text-3xl leading-tight
-             text-red-700 ">
+             
+             text-red-700 "
+          >
             Italian Restaurant
           </h3>
-          <h1
-            className="text-bold text-3xl uppercase font-medium">
-            About us
-          </h1>
+          <h1 className="text-bold text-3xl uppercase font-medium">About us</h1>
           <p className="text-center font-extralight mb-[20px]">
-            Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est.Sed <br />
-            fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi,ac <br />
-            facilisis ligula sem  neque.
+            Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est.Sed{" "}
+            <br />
+            fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi,ac{" "}
+            <br />
+            facilisis ligula sem neque.
           </p>
 
           <a href="/" className="font-medium text-red-900 hover:text-red-600">
             OUR STORY
-          <FontAwesomeIcon className="ml-[5px]" icon={faArrowRight} />
+            <FontAwesomeIcon className="ml-[5px]" icon={faArrowRight} />
           </a>
         </div>
-            
-            <img className="w-[400px] h-[400px] object-fill rounded-3xl" src={about2} alt="" />
-         
-        
+        <div className="relative overflow-hidden w-full lg:w-[400px] h-[400px] rounded-3xl mt-6 lg:mt-0">
+          <img
+            className="w-full h-[400px] object-fill rounded-3xl lg:w-[400px] lg:mt-0"
+            src={about2}
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 flex justify-center items-center text-white hover:opacity-70">
+            <span className="text-xl">
+              <a
+                href=""
+                className="text-red-700 text-2xl hover:text-yellow-100 uppercase underline  "
+              >
+                Reserver
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
