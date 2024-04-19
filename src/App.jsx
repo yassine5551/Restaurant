7da49f9f71eeded3fd5089d2a96086332cfc153b
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-04-19 03:15:19 +0000

    adding slides in hero section and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Bar from './Components/0-bar_top/Bar';
 import Header from "./Components/1-header/Header"
 import Hero from "./Components/2-hero/Hero"
-import About from "./Components/3-about/About"
-import Reservation from "./Components/5-reservation/Reservation"
-import Footer from "./Components/8-footer/Footer"
-import Card from './Components/cards/Card';
-import Discorver from './Components/discorver/Discorver';
+// import About from "./Components/3-about/About"
+// import Reservation from "./Components/5-reservation/Reservation"
+// import Footer from "./Components/8-footer/Footer"
+// import Card from './Components/cards/Card';
+// import Discorver from './Components/discorver/Discorver';
 import './index.css'
 
 
@@ -18,11 +18,11 @@ function App() {
       <Bar/>
       <Header />
       <Hero />
-      <About />
-      <Discorver />
-      <Card />
+      {/* <About /> */}
+      {/* <Discorver /> */}
+      {/* <Card />
       <Reservation />
-      <Footer />  
+      <Footer />   */}
     </>
   )
 }
--- src/Components/2-hero/Hero.jsx
+++ src/Components/2-hero/Hero.jsx
@@ -1,20 +1,155 @@
-import heroImg2 from '/./public/heroImg2.jpg';
+import heroImg2 from "/./public/heroImg2.jpg";
+import s1 from "/./public/s1.jpg";
+import s2 from "/./public/s2.jpg";
+import s3 from "/./public/s3.jpg";
+import s4 from "/./public/s4.jpg";
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function Hero() {
-  
+  // return (
+  //   <>
+  // <div className="relative flex  justify-center items-center ">
+  //   <img src={heroImg2} alt="" />
+  //   <div className="absolute text-white uppercase text-center">
+  //     <h1 className="text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian ">
+  //       Welcome to
+  //     </h1>
+  //     <p className="text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 ">
+  //       pato place
+  //     </p>
+  //     <a
+  //       className="text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light "
+  //       href="/"
+  //     >
+  //       LOOk MENU
+  //     </a>
+  //   </div>
+  // </div>;
+
+  //   </>
+  // )
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
   return (
     <>
-      <div className="relative flex  justify-center items-center ">
-        <img src={heroImg2} alt="" />
-        <div className='absolute text-white uppercase text-center'>
-        <h1 className='text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian '>Welcome to</h1>
-        <p className='text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 '>pato place</p>
-        <a className='text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light ' href="/" >LOOk MENU</a>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        // navigation={false}
+        modules={[Autoplay, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="relative flex  justify-center items-center w-full xl:h-[800px] lg:h-[739px] h-80">
+            <img src={s1} alt="" className=""/>
+            <div className="absolute text-white uppercase text-center">
+              <h1 className="text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian ">
+                Welcome to
+              </h1>
+              <p className="text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 ">
+                pato place
+              </p>
+              <a
+                className="text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light "
+                href="/"
+              >
+                Take A Reservation
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative flex  justify-center items-center w-full xl:h-[800px]  lg:h-[739px] h-80 ">
+            <img src={s2} alt="" className="h-fit" />
+            <div className="absolute text-white uppercase text-center">
+              <h1 className="text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian ">
+                Our speacial menu
+              </h1>
+              <p className="text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 ">
+                pato place
+              </p>
+              <a
+                className="text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light "
+                href="/"
+              >
+                Check Out Our Menu
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative flex  justify-center items-center w-full xl:h-[800px] lg:h-[739px] h-80 ">
+            <img src={s3} alt="" className="h-fit"/>
+            <div className="absolute text-white uppercase text-center">
+              <h1 className="text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian ">
+                Pictures from
+              </h1>
+              <p className="text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 ">
+                pato place
+              </p>
+              <a
+                className="text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light "
+                href="/"
+              >
+                GALLERY
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative flex  justify-center items-center w-full xl:h-[800px] lg:h-[739px] h-80 ">
+            <img src={s4} alt="" className="h-32" />
+            <div className="absolute text-white uppercase text-center">
+              <h1 className="text-xs sm:text-xl md:text-2xl lg:text-5xl xl:text-6xl font-light md:mb-5 lg:mb-6 xl:mb-7 font-italian ">
+                story of
+              </h1>
+              <p className="text-lg sm:text-base md:text-3xl lg:text-7xl xl:text-8xl mb-2 md:mb-5 lg:mb-6 xl:mb-7 ">
+                pato place
+              </p>
+              <a
+                className="text-[12px] sm:text-base  p-1 md:p-3  bg-red-700 hover:bg-red-600 font-light "
+                href="/"
+              >
+                ABOUT US
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <div className="autoplay-progress hidden" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
         </div>
-      </div>
-      
+      </Swiper>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
